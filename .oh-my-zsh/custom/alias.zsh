956ecb832c53193520e3445fce098f98365694ff
D Bruce Saurer <notami@gmail.com>
2018-01-09 09:43:19 -0500

    keybindings & alias to i3

--- .oh-my-zsh/custom/alias.zsh
+++ .oh-my-zsh/custom/alias.zsh
@@ -7,37 +7,46 @@
 # alias zshconfig="mate ~/.zshrc"                                                 
 # alias ohmyzsh="mate ~/.oh-my-zsh"                                               
 
-
-#fix obvious typo's
+#######################
+# #fix obvious typo's #
+#######################
 alias cd..='cd ..'
 alias sl="ls"
 
-## Colorize the grep command output for ease of use (good for log files)##
+##############################################################################
+# ## Colorize the grep command output for ease of use (good for log files)## #
+##############################################################################
 alias pdw="pwd"
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
-#readable output
+####################
+# #readable output #
+####################
 alias df='df -h'
-
 alias merge="xrdb -merge ~/.Xresources"
 
-### general ###
-alias cls="clear"                                                                 
-alias la="ls -alG"                                                                
-alias uls="cd /usr/local/share"                                                   
-alias l.="ls -A | egrep '^\.'"      
-
+###################
+# ### general ### #
+###################
+alias cls="clear" 
+alias la="ls -alG"
+alias uls="cd /usr/local/share"
+alias l.="ls -A | egrep '^\.'"
 alias xpp="xprop | grep -i 'class'" 
 
-### config - (git alias) ###
+################################
+# ### config - (git alias) ### #
+################################
 alias cs="config status"
 alias ca="config add"
 alias cc="config commit -m"
 alias cp="config push"
 
-### dot files ###
+#####################
+# ### dot files ### #
+#####################
 alias v3="vim /home/notami/.i3/config"
 alias vr="vim /home/notami/.config/ranger/rc.conf"
 alias vm="vim /home/notami/.config/mutt/neomuttrc"
@@ -46,31 +55,46 @@ alias vp="vim /home/notami/.vimperatorrc"
 alias vv="vim /home/notami/.vimrc"
 alias vz="vim /home/notami/.zshrc"
 alias vza="vim /home/notami/.oh-my-zsh/custom/alias.zsh"
+alias vi3="vim /home/notami/.config/i3/config"
 
-### nav ###
+###############
+# ### nav ### #
+###############
 alias www="cd /mnt/www/nginx/dev/"
 alias wwn="cd /mnt/www/nginx/notes/articles"
 alias wwr="cd /mnt/www/nginx/refDesk/"
 alias wwt="cd /mnt/www/nginx/tv-home/"
+alias gam="cd /home/notami/Documents/ArchMerge-Nemesis/"
+alias gcf="cd /home/notami/.config"
 
-### ssh ###
+###############
+# ### ssh ### #
+###############
 alias ssh.f="ssh notami@freenas.local"
 alias ssh.d="ssh dbsaurer@dbsaurer.com"
 
-### commands ###
+####################
+# ### commands ### #
+####################
 alias scrimp="scrot -q 85 -d 5 screenshot.png && gimp screenshot.png &"
 
-### iohyve ###
+##################
+# ### iohyve ### #
+##################
 alias io="iohyve"
 
-### tmux ###
+################
+# ### tmux ### #
+################
 alias tmn="tmux new -s"
 alias tma="tmux a -t"
 alias tms="tmux list-sessions"
 alias tmks="tmux kill-session -t"
 alias tmrs="tmux source-file ~/.tmux/.tmux.rsync.conf"
 
-### rsync ###
+#################
+# ### rsync ### #
+#################
 alias rsdbsd="rsync -auvzhe ssh --progress dbsaurer@104.255.96.68:/home/dbsaurer/public_html/ /home/notami/Documents/MyDocuments/Client/DBS/dbsaurer.com/BU-DBS/LocalMirror-DBS/"
 alias rsdbsf="rsync -ruvzhe ssh --progress "/home/notami/Documents/MyDocuments/Client/DBS/" "/mnt/Data/Docs/Client/DBS/""
 alias rsl3d="rsync -auvzhe ssh --progress legacythree@104.255.96.68:/home/legacythree/public_html/ /home/notami/Documents/MyDocuments/Client/LegacyIII/legacythree.org/BU-L3/LocalMirror-L3/"
@@ -95,7 +119,9 @@ alias pksn="packer -S --noconfirm --noedit"
 alias pksyu="packer -Syu  --noconfirm --noedit"
 alias tvstart="systemctl start teamviewerd.service"
 
-# show log output
+#####################
+# # show log output #
+#####################
 alias syslog="journalctl -p 3 -xb"
 
 
@@ -119,7 +145,10 @@ alias ufw='sudo ufw'
 alias ufwsv='sudo ufw status verbose'
 alias ufwsn='sudo ufw status numbered'
 
-#
+################
+# GLOBAL ALIAS #
+################
+
 # http://www.zzapper.co.uk/zshtips.html
 alias -g ND='*(/om[1])'        # newest directory
 alias -g NF='*(.om[1])'        # newest file
